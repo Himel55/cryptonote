9efe07e2591cd9753373e743d4569b2efc30c486
Himel Patel <Himel55@gmail.com>
2018-01-14 18:23:28 +1300

    Update seed node to include local ip

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -107,6 +107,7 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
   "114.23.225.143:17236",
+  "192.168.1.7:17236",
   //"your_seed_ip2.com:8080",
 };
 
